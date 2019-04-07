251e5f33c67da6e472e9d7e7f680a150cdbf6265
samuelpolat <samuelpolat15@gmail.com>
2019-04-07 21:45:06 +0200

    minor improvements

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ exports.trackVisit = function(req) {
         dy = date.getUTCDate();
         hr = date.getUTCHours();
         mn = date.getUTCMinutes();
-        timestamp = [yr, mo, dy, hr, mn];
+        var timestamp = {"y": yr, "m": mo, "d": dy, "hr": hr, "mn": mn};
 
         //Track visit
         db.get('visits')
