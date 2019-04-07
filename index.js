900418c6cf3d80e18cf3b3a99f710a3fcb74e853
samuelpolat <samuelpolat15@gmail.com>
2019-04-07 17:04:51 +0200

    minor improvements

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-/** @param header The postal code */
-
 const bowser = require('bowser');
 const isbot = require('isbot');
 const geoip = require('geoip-lite');
@@ -9,56 +7,60 @@ const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('visits.json')
 const db = low(adapter)
 
-db.defaults({ visits: [], count: 0 })
-  .write()
+db.defaults({
+        visits: [],
+        count: 0
+    })
+    .write()
 
-exports.trackVisit = function (req) {
+exports.trackVisit = function(req) {
 
-  var header = req.headers;
-  var dnt = header['dnt'];
-  var ua = header['user-agent'];
-  var ip = header['x-forwarded-for'] ? header['x-forwarded-for'].split(',')[0] : req.connection.remoteAddress;
-  var bot = isbot(ua);
+    var header = req.headers;
+    var dnt = header['dnt'];
+    var ua = header['user-agent'];
+    var ip = header['x-forwarded-for'] ? header['x-forwarded-for'].split(',')[0] : req.connection.remoteAddress;
+    var bot = isbot(ua);
 
-  // Check DNT-header and detect bots
-  if (dnt != '1' && bot != true) {
+    // Check DNT-header and detect bots
+    if (dnt != '1' && bot != true) {
 
-    var client = bowser.getParser(ua);
+        var client = bowser.getParser(ua);
 
-    // Client technology
-    var browser = client.getBrowser();
-    var os = client.getOS();
-    var device = client.getPlatformType();
+        // Client technology
+        var browser = client.getBrowser();
+        var os = client.getOS();
+        var device = client.getPlatformType();
 
-    //Client location
-    var geo = geoip.lookup(ip);
-    var country = geo['country'];
-    var region = geo['region'];
-    var city = geo['city'];
+        //Client location
+        var geo = geoip.lookup(ip);
+        var country = geo['country'];
+        var region = geo['region'];
+        var city = geo['city'];
 
-    //Add timestamp
-    var timestamp = Math.floor(Date.now() / 1000);
+        //Add timestamp
+        var timestamp = Math.floor(Date.now() / 1000);
 
-    //Track visit
-    db.get('visits')
-      .push({ browser: browser,
-              os: os,
-              device: device,
-              country: country,
-              region: region,
-              city: city,
-              timestamp: timestamp
+        //Track visit
+        db.get('visits')
+            .push({
+                browser: browser,
+                os: os,
+                device: device,
+                country: country,
+                region: region,
+                city: city,
+                timestamp: timestamp
             })
-      .write()
+            .write()
 
-    db.update('count', n => n + 1)
-      .write()
-      
-  } else {
+        db.update('count', n => n + 1)
+            .write()
 
-    // User does not consent to tracking
-    // or might be a bot
+    } else {
 
-  }
+        // User does not consent to tracking
+        // or might be a bot
+
+    }
 
 };
