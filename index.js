71f9cdac9a85ecdb1b656ff415df00e6424dd669
samuelpolat <samuelpolat15@gmail.com>
2019-04-07 12:22:49 +0200

    basic tracking

---
+++ index.js
@@ -0,0 +1,34 @@
+const bowser = require('bowser');
+const isbot = require('isbot');
+
+exports.trackVisit = function() {
+
+  var header = req.headers;
+  var dnt = header['dnt'];
+  var ua = header['user-agent'];
+  var bot = isbot(ua);
+
+  // Check DNT-header and detect bots
+  if (dnt != '1' && bot != true) {
+
+    var client = bowser.getParser(ua);
+
+    // Client technology
+    var browser = client.getBrowser();
+    var os = client.getOS;
+    var device = client.getPlatformType();
+
+    //Track visit
+    var json = '{'browser': browser, 'os': os, 'device': device}';
+    visit = JSON.parse(json);
+
+    console.log(visit);
+
+  } else {
+
+    // User does not consent to tracking
+    // or might be a bot
+
+  }
+
+}
