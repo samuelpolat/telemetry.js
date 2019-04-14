8ab453b06f6adfb3631639e52e959f875a850955
samuelpolat <samuelpolat15@gmail.com>
2019-04-14 17:18:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,117 +7,121 @@ module.exports = function(options) {
   var dbEnabled = options.enableMongo;
   var connection = options.connection;
 
-  trackVisit = function(req) {
+  return {
 
-    var header = req.headers;
-    var dnt = header['dnt'];
-    var ua = header['user-agent'];
-    var ip = header['x-forwarded-for'] ? header['x-forwarded-for'].split(',')[0] : req.connection.remoteAddress;
-    var bot = isbot(ua);
+    trackVisit: function(req) {
 
-    // Check DNT-header and detect bots
-    if (dnt != '1' && bot != true & req.originalUrl != '/favicon.ico') {
+      var header = req.headers;
+      var dnt = header['dnt'];
+      var ua = header['user-agent'];
+      var ip = header['x-forwarded-for'] ? header['x-forwarded-for'].split(',')[0] : req.connection.remoteAddress;
+      var bot = isbot(ua);
 
-        var client = bowser.getParser(ua);
+      // Check DNT-header and detect bots
+      if (dnt != '1' && bot != true & req.originalUrl != '/favicon.ico') {
 
-        //Traffic source
-        var path = req.originalUrl;
-        var referer = header['referer'];
+          var client = bowser.getParser(ua);
 
-        // Client technology
-        var browser = client.getBrowser();
-        var os = client.getOS();
-        var device = client.getPlatformType();
+          //Traffic source
+          var path = req.originalUrl;
+          var referer = header['referer'];
 
-        //Client location
-        var geo = geoip.lookup(ip);
-        var country = geo['country'];
-        var region = geo['region'];
-        var city = geo['city'];
+          // Client technology
+          var browser = client.getBrowser();
+          var os = client.getOS();
+          var device = client.getPlatformType();
 
-        //Create visit array
-        var data = [path, referer, browser, os, device, country, region, city];
+          //Client location
+          var geo = geoip.lookup(ip);
+          var country = geo['country'];
+          var region = geo['region'];
+          var city = geo['city'];
 
-        //Track visit
-        if (dbEnabled == true && connection.length > 0) {
-          saveMongo(data);
-        } else {
-          saveJSON(data);
-        };
+          //Create visit array
+          var data = [path, referer, browser, os, device, country, region, city];
 
-      } else {}
+          //Track visit
+          if (dbEnabled == true && connection.length > 0) {
+            saveMongo(data);
+          } else {
+            saveJSON(data);
+          };
 
-  };
+        } else {}
 
-  //Storage options:
+    };
 
-  saveJSON = function(data) {
+    //Storage options:
 
-    const low = require('lowdb');
-    const FileSync = require('lowdb/adapters/FileSync');
-    const adapter = new FileSync('visits.json');
-    const db = low(adapter);
+    saveJSON: function(data) {
 
-    db.defaults({ visits: [] })
-        .write()
+      const low = require('lowdb');
+      const FileSync = require('lowdb/adapters/FileSync');
+      const adapter = new FileSync('visits.json');
+      const db = low(adapter);
 
-    //Create timestamp
-    var timestamp = Math.floor(Date.now() / 1000);
+      db.defaults({ visits: [] })
+          .write()
 
-    db.get('visits')
-        .push({
-            path: data[0],
-            referer: data[1],
-            browser: data[2],
-            os: data[3],
-            device: data[4],
-            country: data[5],
-            region: data[6],
-            city: data[7],
-            timestamp: timestamp
-        })
-        .write()
+      //Create timestamp
+      var timestamp = Math.floor(Date.now() / 1000);
 
-  };
+      db.get('visits')
+          .push({
+              path: data[0],
+              referer: data[1],
+              browser: data[2],
+              os: data[3],
+              device: data[4],
+              country: data[5],
+              region: data[6],
+              city: data[7],
+              timestamp: timestamp
+          })
+          .write()
 
-  saveMongo = function(data) {
+    };
 
-    var mongoose = require('mongoose');
-    mongoose.connect(connection, {useNewUrlParser: true});
+    saveMongo: function(data) {
 
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', function() {
-      // we're connected!
-    });
+      var mongoose = require('mongoose');
+      mongoose.connect(connection, {useNewUrlParser: true});
 
-    var object = mongoose.model('visit', {
-        path: String,
-        referer: String,
-        browser: [{ name: String, version: String }],
-        os: [{ name: String, version: String }],
-        device: String,
-        country: String,
-        region: String,
-        city: String
-    }, {
-      timestamps: true
-    });
+      var db = mongoose.connection;
+      db.on('error', console.error.bind(console, 'connection error:'));
+      db.once('open', function() {
+        // we're connected!
+      });
 
-    var visit = new object({
-        path: data[0],
-        referer: data[1],
-        browser: data[2],
-        os: data[3],
-        device: data[4],
-        country: data[5],
-        region: data[6],
-        city: data[7]
-    });
+      var object = mongoose.model('visit', {
+          path: String,
+          referer: String,
+          browser: [{ name: String, version: String }],
+          os: [{ name: String, version: String }],
+          device: String,
+          country: String,
+          region: String,
+          city: String
+      }, {
+        timestamps: true
+      });
 
-    visit.save(function (err, visit) {
-      if (err) return console.error(err);
-    });
+      var visit = new object({
+          path: data[0],
+          referer: data[1],
+          browser: data[2],
+          os: data[3],
+          device: data[4],
+          country: data[5],
+          region: data[6],
+          city: data[7]
+      });
+
+      visit.save(function (err, visit) {
+        if (err) return console.error(err);
+      });
+
+    };
 
   };
 
